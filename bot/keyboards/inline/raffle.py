ce94a447d6e2d3fe21aec0caa0b0c8a7860129b7
Andrew <vesell4123@mail.ru>
2023-02-13 08:27:18 +0300

    create inline raffle buttons to raffle

---
+++ bot/keyboards/inline/raffle.py
@@ -0,0 +1,20 @@
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+async def set_raffle_menu() -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    builder.row(
+        InlineKeyboardButton(text="Участвовать", callback_data="become_member"),
+        InlineKeyboardButton(text="Текущий приз", callback_data="current_prize"),
+        InlineKeyboardButton(text="Победители", callback_data="winners")
+    )
+    return builder.as_markup(resize_keyboard=True)
+
+
+async def back_to_raffle_menu() -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    builder.row(
+        InlineKeyboardButton(text="Назад", callback_data="back_raffle")
+    )
+    return builder.as_markup(resize_keyboard=True)
