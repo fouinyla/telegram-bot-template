a1511d1dc7cf38c5910658988d6917cc3f08c44a
Andrew <vesell4123@mail.ru>
2023-02-14 14:38:53 +0300

    move buttons

--- bot/keyboards/inline/payments.py
+++ bot/keyboards/inline/payments.py
@@ -17,13 +17,13 @@ async def payment_methods(amount: str) -> InlineKeyboardMarkup:
 async def amount_money() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.row(
-        InlineKeyboardButton(text="100р", callback_data=":100"),
-        InlineKeyboardButton(text="200р", callback_data=":200")
+        InlineKeyboardButton(text="3000р", callback_data=":3000"),
+        InlineKeyboardButton(text="1000р", callback_data=":1000"),
+        InlineKeyboardButton(text="500р", callback_data=":500"),
     )
     builder.row(
-        InlineKeyboardButton(text="500р", callback_data=":500"),
-        InlineKeyboardButton(text="1000р", callback_data=":1000"),
-        InlineKeyboardButton(text="3000р", callback_data=":3000")
+        InlineKeyboardButton(text="200р", callback_data=":200"),
+        InlineKeyboardButton(text="100р", callback_data=":100")
     )
     builder.row(
         InlineKeyboardButton(text="Назад", callback_data="back_raffle")
