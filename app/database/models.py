55c9f5230a378ecb22a372dc1f8fc1d4280f0bb6
Andrew <vesell4123@mail.ru>
2023-02-14 03:46:34 +0300

    replace integer to biginteger

--- app/database/models.py
+++ app/database/models.py
@@ -1,12 +1,12 @@
 from sqlalchemy import (
     Column,
-    Integer,
     String,
     ForeignKey,
     DateTime,
     Boolean,
     Text,
-    Float
+    Float,
+    BigInteger
 )
 from sqlalchemy.orm import declarative_base
 from bot.time import get_moscow_datetime
@@ -18,9 +18,9 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
-    id: int = Column(Integer, primary_key=True, nullable=False)
+    id: int = Column(BigInteger, primary_key=True, nullable=False)
     name: str = Column(String(50), nullable=False)
-    tg_id: int = Column(Integer, nullable=False)
+    tg_id: int = Column(BigInteger, nullable=False)
     tg_username: str = Column(String(32), nullable=True)
     date_of_registry: datetime = Column(DateTime, default=get_moscow_datetime())
     is_admin: bool = Column(Boolean, nullable=False, default=False)
@@ -28,15 +28,15 @@ class User(Base):
 
 class Raffle(Base):
     __tablename__ = "raffle"
-    id: int = Column(Integer, primary_key=True, nullable=False)
-    user_id: int = Column(Integer, ForeignKey("users.id"), nullable=False)
+    id: int = Column(BigInteger, primary_key=True, nullable=False)
+    user_id: int = Column(BigInteger, ForeignKey("users.id"), nullable=False)
     donated: float = Column(Float, nullable=False)
 
 
 class Lot(Base):
     __tablename__ = "lots"
-    id: int = Column(Integer, primary_key=True, nullable=False)
-    user_id: int = Column(Integer, ForeignKey("users.id"), nullable=False)
+    id: int = Column(BigInteger, primary_key=True, nullable=False)
+    user_id: int = Column(BigInteger, ForeignKey("users.id"), nullable=False)
     name: str = Column(String(15), nullable=False)
     title: str = Column(String(25), nullable=False)
     problem: str = Column(Text, nullable=False)
@@ -47,15 +47,15 @@ class Lot(Base):
 
 class UserLot(Base):
     __tablename__ = 'user_lots'
-    id: int = Column(Integer, primary_key=True, nullable=False)
-    user_id: int = Column(Integer, ForeignKey("users.id"), nullable=False)
-    lot_id: int = Column(Integer, ForeignKey("users.id"), nullable=False)
+    id: int = Column(BigInteger, primary_key=True, nullable=False)
+    user_id: int = Column(BigInteger, ForeignKey("users.id"), nullable=False)
+    lot_id: int = Column(BigInteger, ForeignKey("users.id"), nullable=False)
     donated: float = Column(Float, nullable=False)
 
 
 class Winner(Base):
     __tablename__ = 'winners'
-    id: int = Column(Integer, primary_key=True, nullable=False)
-    user_id: int = Column(Integer, ForeignKey("users.id"), nullable=False)
+    id: int = Column(BigInteger, primary_key=True, nullable=False)
+    user_id: int = Column(BigInteger, ForeignKey("users.id"), nullable=False)
     date_of_victory: datetime = Column(DateTime, nullable=False)
     prize: float = Column(Float, nullable=False)
