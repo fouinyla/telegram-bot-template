ba16c0b1981e691fab1a07f106aaea433b422c5a
Andrew <vesell4123@mail.ru>
2023-02-16 15:39:53 +0300

    add sqlalchemy serializer

--- app/serializers.py
+++ app/serializers.py
@@ -1,4 +1,4 @@
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
 from app.database.models import User
 
--- app/server.py
+++ app/server.py
@@ -1,9 +1,11 @@
+import logging
+
 from fastapi import FastAPI, Request, Depends
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy.ext.asyncio import AsyncSession
 from aiogram.types import Update
 
-from settings import DEBUG, BASE_DIR
+from settings import DEBUG
 from bot.bot import dp, bot
 from app.database import get_session
 from app.routers import admin
@@ -18,7 +20,7 @@ app = FastAPI(
     docs_url=None,
     redoc_url=None,
 )
-print(BASE_DIR)
+
 app.mount(
     "/static",
     StaticFiles(directory=f"static"),
@@ -39,7 +41,6 @@ async def process_update(request: Request, session: AsyncSession = Depends(get_s
         await dp.feed_update(bot=bot, update=update, session=session)
     except ValueError:
         logging.warning("body", await request.body())
-
 
 
 app.include_router(
