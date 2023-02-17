c877e8aa40a38d1181b0607cd5feca72e69207cf
Andrew <vesell4123@mail.ru>
2023-02-17 04:20:15 +0300

    use yoomoney link for payment

--- bot/keyboards/inline/payments.py
+++ bot/keyboards/inline/payments.py
@@ -1,11 +1,22 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+from bot.libs.yoomoney.quickpay import Quickpay
+
 
 async def payment_methods(amount: str) -> InlineKeyboardMarkup:
+    quickpay = Quickpay(
+        receiver="4100118112830579",
+        quickpay_form="shop",
+        targets="Sponsor this project",
+        paymentType="SB",
+        sum=float(amount[1:]),
+        label="a1b2c3d4e5"
+    )
+
     builder = InlineKeyboardBuilder()
     builder.row(
-        InlineKeyboardButton(text="ЮMoney", callback_data="UMoney" + amount),
+        InlineKeyboardButton(text="ЮMoney", url=str(quickpay.redirected_url), callback_data="UMoney" + amount),
         InlineKeyboardButton(text="Крипта", callback_data="Crypto" + amount)
     )
     builder.row(
