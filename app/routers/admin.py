d6f440160748a26f3020bf11a0e56e5e1d103e49
Andrew <vesell4123@mail.ru>
2023-02-19 03:11:36 +0300

    added sending messages in the background via the admin panel

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, Depends, status, HTTPException
+from fastapi import APIRouter, Request, Depends, status
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, JSONResponse
 from sqlalchemy import select
@@ -8,6 +8,8 @@ from app.database import get_session
 from app.database.models import User
 from app.schemas import MessageSchema
 from app.serializers import UserSerializer
+
+from app.celery.tasks import send_message_everyone_task
 
 
 templates = Jinja2Templates(directory="templates")
@@ -22,11 +24,12 @@ async def get_menu_template(request: Request):
 
 @router.post("/sendmessage")
 async def send_message_to_everyone(message: MessageSchema, session: AsyncSession = Depends(get_session)):
-    # raise HTTPException(status_code=404, detail="Smth went wrong")
     user_ids = (await session.execute(
-        select(User.tg_id)\
+        select(User.tg_id)
         .where(User.is_admin.__ne__(True))
     )).all()
     user_serializer = UserSerializer()
     user_ids = user_serializer.dump(user_ids, many=True)
+    for user in user_ids:
+        send_message_everyone_task.delay(chat_id=user["tg_id"], text=message.text)
     return JSONResponse(status_code=status.HTTP_200_OK, content="ok")
