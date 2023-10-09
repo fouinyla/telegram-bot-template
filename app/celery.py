14d5f9300bf06bc057799b7ee299fb378868f38a
oleh <mefouiny@gmail.com>
2023-10-09 13:12:30 +0300

    edit celery config: add related_name to autodiscover_tasks function

--- app/celery.py
+++ app/celery.py
@@ -11,9 +11,13 @@ celery_app = Celery(
     broker=redis_settings.url,
 )
 celery_app.conf.beat_schedule = {
-    'test': {
-        'task': 'bot.background_tasks.periodic.test_task.test_task',
-        'schedule': crontab(),
+    "test": {
+        "task": "bot.background_tasks.periodic.test_task.test_task",
+        "schedule": crontab(),
     }
 }
-celery_app.autodiscover_tasks(['bot.background_tasks', 'bot.background_tasks.periodic'], force=True)
+celery_app.autodiscover_tasks(
+    ["bot.background_tasks", "bot.background_tasks.periodic"],
+    force=True,
+    related_name="periodic",
+)
